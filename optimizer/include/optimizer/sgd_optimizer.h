a0a4e97032eda032742b1dd47a5aac56d1052067
jinsheng <yxjxmdx888@163.com>
2022-05-23 21:33:14 +0800

    sgd

--- optimizer/include/optimizer/sgd_optimizer.h
+++ optimizer/include/optimizer/sgd_optimizer.h
@@ -2,7 +2,27 @@
 // Created by cleve on 5/12/2022.
 //
 
-#ifndef CPPBP_OPTIMIZER_INCLUDE_OPTIMIZER_SGD_OPTIMIZER_H_
-#define CPPBP_OPTIMIZER_INCLUDE_OPTIMIZER_SGD_OPTIMIZER_H_
+#pragma once
 
-#endif //CPPBP_OPTIMIZER_INCLUDE_OPTIMIZER_SGD_OPTIMIZER_H_
+#include <optimizer/optimizer.h>
+
+#include <Eigen/Eigen>
+
+#include <cstdint>
+#include "Eigen/Core"
+
+namespace cppbp::optimizer
+{
+class Sgd_optimizer
+	:public  IOptimizer
+{
+ public:
+	explicit Sgd_optimizer(double lr);
+
+	void step() override;
+	Eigen::MatrixXd optimize(Eigen::MatrixXd prev, Eigen::MatrixXd grads) override;
+ private:
+	uint64_t step_{};
+	double lr_{};
+};
+}
