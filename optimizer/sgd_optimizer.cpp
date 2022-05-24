d6f63224e3ea60f00e604e24bdf9831c76eefcc8
jinsheng <yxjxmdx888@163.com>
2022-05-25 01:08:29 +0800

    implement the simplest sgd

--- optimizer/sgd_optimizer.cpp
+++ optimizer/sgd_optimizer.cpp
@@ -15,7 +15,28 @@ void cppbp::optimizer::Sgd_optimizer::step()
 {
 	step_++;
 }
+/*
+ * Eigen::MatrixXd
+ * element size : dynamic
+ * element type : double
+ * */
 Eigen::MatrixXd cppbp::optimizer::Sgd_optimizer::optimize(Eigen::MatrixXd prev, Eigen::MatrixXd grads)
-{
+{	//
+	Eigen::VectorXd g = Eigen::VectorXd::Ones(prev.rows());
 
+	for (int i = 0; i < prev.rows()/10; ++i)
+	{
+		int k = rand()%prev.rows()+0;
+		g[k] = 1;
+	}
+
+	for (int i = 0; i < g.size(); ++i)
+	{
+		if(g(i) == 0)
+		{
+			for(int j = 0;j < prev.cols();j++)
+				grads(i,j) = 0;
+		}
+	}
+	return prev + lr_ * grads;
 }
