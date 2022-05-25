0c775034fbfcad73e6c4e5811f442fa29b9fed14
SmartPolarBear <CleverCoolBear@outlook.com>
2022-05-25 15:04:38 +0800

    feat(dataset): add regularization to iris dataset

--- dataloader/iris_dataset.cpp
+++ dataloader/iris_dataset.cpp
@@ -4,6 +4,7 @@
 #include <dataloader/iris_dataset.h>
 
 #include <algorithm>
+#include <iostream>
 
 using namespace cppbp::dataloader;
 
@@ -12,12 +13,17 @@ using namespace std;
 using namespace Eigen;
 
 cppbp::dataloader::IrisDataset::IrisDataset(std::string pathname)
-	: path_(std::move(pathname))
+	: IDataset(), path_(std::move(pathname))
 {
 	csv2::Reader<csv2::delimiter<','>,
 				 csv2::quote_character<'"'>,
 				 csv2::first_row_is_header<false>,
-				 csv2::trim_policy::trim_whitespace> csv{};
+				 csv2::trim_policy::trim_whitespace>
+		csv{};
+
+	VectorXd sum = VectorXd::Zero(4);
+	VectorXd squared = VectorXd::Zero(4);
+
 	if (csv.mmap(path_))
 	{
 		for (const auto& row : csv)
@@ -43,7 +49,21 @@ cppbp::dataloader::IrisDataset::IrisDataset(std::string pathname)
 			}
 
 			data_.emplace_back(params, label);
+
+			sum += params;
+			squared += params.cwiseProduct(params);
 		}
+	}
+
+	sum /= data_.size();
+	squared /= data_.size();
+
+	VectorXd variance((squared - sum.cwiseProduct(sum)).array().sqrt());
+
+	for (auto& d : data_)
+	{
+		d.first -= sum;
+		d.first = d.first.cwiseQuotient(variance);
 	}
 }
 
@@ -56,5 +76,3 @@ size_t cppbp::dataloader::IrisDataset::size() const
 {
 	return data_.size();
 }
-
-
