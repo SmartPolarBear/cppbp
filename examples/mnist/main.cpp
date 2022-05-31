c01cb0693f509f7aa5363917295d888a3dbc3f77
SmartPolarBear <CleverCoolBear@outlook.com>
2022-05-31 16:13:02 +0800

    feat(examples): fix mnist

--- examples/mnist/main.cpp
+++ examples/mnist/main.cpp
@@ -69,13 +69,12 @@ int main()
 
     Input in{784};
     FullyConnected fc1{800, relu};
-    FullyConnected fc2{700, relu};
-    FullyConnected fc3{400, relu};
-    FullyConnected fc4{80, sigmoid};
+    FullyConnected fc2{400, relu};
+    FullyConnected fc3{80, sigmoid};
     FullyConnected out{10, softmax};
 
     CrossEntropyLoss loss{};
-    Model model{in | fc1 | fc2 | fc3 | fc4 | out, loss};
+    Model model{in | fc1 | fc2 | fc3 | out, loss};
 
     MNISTDataset mnist{"data/train-labels.idx1-ubyte", "data/train-images.idx3-ubyte", true};
     MNISTDataset mnist_test{"data/t10k-labels.idx1-ubyte", "data/t10k-images.idx3-ubyte", true};
@@ -89,15 +88,12 @@ int main()
     auto top_3_accuracy_callback = IModelCallback::make<AccuracyCallback>(vector<int>{1, 3});
 
     vector<shared_ptr<IModelCallback>> callbacks{loss_output_callback, accuracy_callback};
-    model.fit(dl, 1000, optimizer, true, 100, callbacks);
+    model.fit(dl, 600, optimizer, true, 100, callbacks);
 
-    DataLoader eval_dl{mnist_test, 16, true};
-    vector<shared_ptr<IModelCallback>> eval_callbacks{loss_output_callback, top_3_accuracy_callback};
-    model.evaluate(eval_dl, true, eval_callbacks);
 
     auto result = model.predict(mnist_test);
 
-    for (int i = 0; i < mnist_test.size(); i++)
+    for (int i = 0; i < mnist_test.size(); i += 100)
     {
         auto [data, label] = mnist_test.get(i);
         print_result(label, result[i]);
